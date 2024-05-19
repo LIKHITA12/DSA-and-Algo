7155b99c3cbbe7ad811f2b777f646d21ff5dcb3e
Likhita C <42487404+LIKHITA12@users.noreply.github.com>
2024-05-19 22:42:39 +0530

    Update Array_Manipulation.cpp

--- Easy Problems/Array_Manipulation.cpp
+++ Easy Problems/Array_Manipulation.cpp
@@ -1,1 +1,23 @@
-i
+// To find the number present in a vector 
+
+class Solution {
+public:
+    int FindUnique(vector<int>& nums) {
+
+        int unique = 0;
+        for(int i=0; i<nums.size();i++){
+            for (int j=i+1; j<nums.size(); j++){
+                if(nums[i] == nums[j]){
+                    nums[i] = nums[j] = -1;
+                }  
+            }
+        }
+        for(int i=0; i<nums.size(); i++){
+          if(nums[i] > 0){
+            unique = nums[i];
+          }
+        }
+        return unique;
+
+    }
+};
